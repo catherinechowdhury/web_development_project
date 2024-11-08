a951012fb7f55330a6084265797c05405a3b55ee
chowdhury_cat <luna2soriano@gmail.com>
2024-11-07 21:29:49 -0500

    create javascript

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-alert("Hello World")
-console.log(1+1)
+//BMI Calculation
+let weight= 130;
+let height= 64;
+let bmi= (weight*703)/(height**2);
+let healthstate
+
+if(bmi<18.5){
+    healthstate="underweight";
+}else if(bmi<=25){
+    healthstate="normal";
+}else if(bmi<=30){
+    healthstate="overweight"
+}else{
+    healthstate="obese"
+}
+
+console.log(`BMI=${bmi.toFixed(2)}. Healthstate = ${healthstate}.`)
