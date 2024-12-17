7c10fb333735e1c9f86340bd6460361668b4c735
chowdhury_cat <luna2soriano@gmail.com>
2024-12-17 11:00:45 -0500

    update main.js

--- public/script/main.js
+++ public/script/main.js
@@ -1,4 +1,21 @@
+const { json } = require("express");
+
 //Login
+async function fetchData(route = '', data = {}, methodType) {
+    const response = await fetch(`http://localhost:3000${route}`, {
+      method: "POST", // *POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(application/json) // body data type must match "Content-Type" header
+    });
+    if (response.ok) {
+      return await response.json(); // parses JSON response into native JavaScript objects
+    } else {
+      throw await response.json();
+    }
+  }
+  
 
 let login= document.getElementById("login")
 if(login) login.addEventListener('submit',logForm)
@@ -9,7 +26,18 @@ function logForm(e){
         username: document.getElementById("username").value,
         password: document.getElementById("password").value
     }
-    console.log(user)
+    // console.log(user)
+
+    fetchData('/user/login',user,"POST")
+    .then(data =>{
+        if(!data.message){
+            window.location.href="index.html"
+        }
+    })
+    .catch(err =>{
+        let errorSection=document.querySelector("#login.error")
+        errorSection.innerText=err.message
+    })
 }
 
 //Register 
